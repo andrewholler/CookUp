976ce801c467ff00ac2951d1166f6520a0eec602
aholler2 <aholler2@illinois.edu>
2018-03-26 15:15:21 -0500

    working index page

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+#from .models import Greeting
 
 # Create your views here.
 def index(request):
@@ -9,11 +9,11 @@ def index(request):
     return render(request, 'index.html')
 
 
-def db(request):
+'''def db(request):
 
     greeting = Greeting()
     greeting.save()
 
     greetings = Greeting.objects.all()
 
-return render(request, 'db.html', {'greetings': greetings})
+    return render(request, 'db.html', {'greetings': greetings})'''
