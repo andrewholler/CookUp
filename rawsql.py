989fd9d8048feb83c82633f1f76694c3f07a2bfc
aholler2 <aholler2@illinois.edu>
2018-03-27 13:32:02 -0500

    modified rawsql and added Pipfile.lock

--- rawsql.py
+++ rawsql.py
@@ -25,7 +25,7 @@ cur.execute("""INSERT INTO recipes (name, appliances, description, instructions,
   ('Baked Teriyaki Chicken', 'saucepan,baking dish,oven,brush', 'A much requested chicken recipe! Easy to double for a large group. Delicious!', 'In a small saucepan over low heat, combine the cornstarch, cold water, sugar, soy sauce, vinegar, garlic, ginger and ground black pepper. Let simmer, stirring frequently, until sauce thickens and bubbles.%%%Preheat oven to 425 degrees F (220 degrees C).%%%Place chicken pieces in a lightly greased 9x13 inch baking dish. Brush chicken with the sauce. Turn pieces over, and brush again.%%%Bake in the preheated oven for 30 minutes. Turn pieces over, and bake for another 30 minutes, until no longer pink and juices run clear. Brush with sauce every 10 minutes during cooking.', 4.5, 60, 6);
 """)
 '''
-
+'''
 cur.execute("""INSERT INTO recipecontains (iid_id, rid_id, amount) VALUES
   (1,1,'1 tablespoon'),
   (2,1,'1 tablespoon'),
@@ -37,7 +37,7 @@ cur.execute("""INSERT INTO recipecontains (iid_id, rid_id, amount) VALUES
   (8,1,'1/4 teaspoon'),
   (9,1,'12');
   """)
-
+'''
 '''
 cur.execute("""CREATE TABLE Ingredient (
 	IID		serial,
