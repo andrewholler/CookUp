e9cc697e8945a3393126f357f1d7691b737ee371
Apoorva <dixit5@illinois.edu>
2018-03-26 20:31:47 -0500

    added register to views.py

--- app/views.py
+++ app/views.py
@@ -5,7 +5,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
 def register(request):
