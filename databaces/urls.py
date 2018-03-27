0064f857f33b5fd630f4edb56b79acd30cfe256d
Apoorva <dixit5@illinois.edu>
2018-03-26 21:58:47 -0500

    fix

--- databaces/urls.py
+++ databaces/urls.py
@@ -12,23 +12,25 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+    """
 
-from django.conf.urls import include, url
-from django.urls import path
+    from django.conf.urls import include, url
+    from django.urls import path
 
-from django.contrib import admin
-admin.autodiscover()
+    from django.contrib import admin
+    admin.autodiscover()
 
-import app.views
+    import app.views
 
 # Examples:
 # url(r'^$', 'databaces.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', app.views.index, name='index'),
-    url(r'^register.html$', app.views.register, name='register'),
+url(r'^$', app.views.index, name='index'),
+url(r'^index.html$', app.views.register, name='register'),
+
+url(r'^register.html$', app.views.register, name='register'),
     # url(r'^db', app.views.db, name='db'),
     path('admin/', admin.site.urls),
-]
+    ]
