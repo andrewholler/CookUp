33d4e1d3babc9c2399888a30dcfe9f329a400163
kpunhani <kanikapunhani@wirelessprv-10-195-248-215.near.illinois.edu>
2018-03-26 21:20:12 -0500

    register up

--- databaces/urls.py
+++ databaces/urls.py
@@ -28,7 +28,7 @@ import app.views
 
 urlpatterns = [
     url(r'^$', app.views.index, name='index'),
-    url(r'^$', app.views.register, name='register'),
+    url(r'^register.html$', app.views.register, name='register'),
     # url(r'^db', app.views.db, name='db'),
     path('admin/', admin.site.urls),
 ]
