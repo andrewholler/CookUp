9bf7e2ecae92badd7956a4b6ddfb14a078be1537
Apoorva <dixit5@illinois.edu>
2018-03-27 22:31:30 -0500

    users inserted properly

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import psycopg2
+from psycopg2 import connect
+import sys
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
 def index(request):
 	print(request.POST.get('email'))
 	if (request.method == "POST"):
@@ -9,8 +14,21 @@ def index(request):
 		return render(request, 'index.html')
 
 def register(request):
-	print(request.POST)
+	# print(request.POST)
+	type(request.POST)
+	myDict = dict(request.POST)
+	# print(myDict)
 	if (request.method == "POST"):
+		con = None
+		con = connect(user='fyrxqvffutzuth', host='ec2-174-129-26-203.compute-1.amazonaws.com', password='81fd164e25fc7569030612fa5a67d1460e534db4289aeef761114c6746429d9b', dbname='d1au6je7k25ijn', port='5432')
+		cur = con.cursor()
+		print("""INSERT INTO Users (firstname, lastname, username, email, password) VALUES
+   						('"""+myDict.get('firstname')[0]+"""','"""+myDict.get('lastname')[0]+"""','"""+myDict.get('username')[0]+"""','"""+myDict.get('email')[0]+"""','"""+myDict.get('password')[0]+"""');""")
+		cur.execute("""INSERT INTO Users (firstname, lastname, username, email, password) VALUES
+  						('"""+myDict.get('firstname')[0]+"""','"""+myDict.get('lastname')[0]+"""','"""+myDict.get('username')[0]+"""','"""+myDict.get('email')[0]+"""','"""+myDict.get('password')[0]+"""');""")
+		con.commit()
+		cur.close()
+		con.close()
 		return render(request, 'dashboard.html')
 	else:
 		return render(request, 'register.html')
@@ -20,14 +38,3 @@ def dashboard(request):
 
 def profile(request):
 	return render(request, 'profile.html')
-
-
-
-'''def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})'''
--- databaces/settings.py
+++ databaces/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = 'databaces.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'd1au6je7k25ijn',
+       'USER': 'fyrxqvffutzuth',
+       'PASSWORD': '81fd164e25fc7569030612fa5a67d1460e534db4289aeef761114c6746429d9b',
+       'HOST': 'ec2-174-129-26-203.compute-1.amazonaws.com',
+       'PORT': '5432',
+   }
 }
 
 
